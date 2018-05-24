sbellem/HoneyBadgerBFT

Port to Python 3

honeybadgerbft/crypto/threshsig/generate_keys.py
@@ -1,6 +1,6 @@
-from boldyreva import dealer, serialize, group
+from .boldyreva import dealer, serialize, group
 import argparse
-import cPickle
+import pickle
 
 def main():
     """ """
@@ -16,7 +16,7 @@ def main():
     PK, SKs = dealer(players=players, k=k)
     content = (PK.l, PK.k, serialize(PK.VK), [serialize(VKp) for VKp in PK.VKs],
                [(SK.i, serialize(SK.SK)) for SK in SKs])
-    print cPickle.dumps(content)
+    print(pickle.dumps(content))
 
 if __name__ == '__main__':
     main()

sbellem/HoneyBadgerBFT-Python

honeybadgerbft/crypto/threshsig/generate_keys.py
@@ -1,13 +1,13 @@
-from boldyreva import dealer, serialize
+from .boldyreva import dealer, serialize
 import argparse
-import cPickle
+import pickle
 
 
 def _generate_keys(players, k):
     if k:
         k = int(k)
     else:
-        k = players / 2  # N - 2 * t
+        k = players // 2  # N - 2 * t
     PK, SKs = dealer(players=players, k=k)
     return (PK.l, PK.k, serialize(PK.VK), [serialize(VKp) for VKp in PK.VKs],
             [(SK.i, serialize(SK.SK)) for SK in SKs])
@@ -20,7 +20,7 @@ def main():
     parser.add_argument('k', help='k')
     args = parser.parse_args()
     keys = _generate_keys(int(args.players), args.k)
-    print cPickle.dumps(keys)
+    print(pickle.dumps(keys))
 
 
 if __name__ == '__main__':
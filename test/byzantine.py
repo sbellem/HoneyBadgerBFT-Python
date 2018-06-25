23ecf1e7f6e19cebe8fcc868b82aed0825fbffec
Sylvain Bellemare <sbellem@gmail.com>
2018-06-25 14:05:39 +0100

    Add test for attack/issue #59

---
+++ test/byzantine.py
@@ -0,0 +1,160 @@
+from collections import defaultdict
+import logging
+
+import gevent
+from gevent.event import Event
+
+from honeybadgerbft.exceptions import RedundantMessageError
+
+
+logger = logging.getLogger(__name__)
+
+
+def byz_ba_issue_59(sid, pid, N, f, coin, input, decide, broadcast, receive):
+    """Modified binary consensus from [MMR14], so that it exhibits a
+    byzantine behavior as per issue #59
+    (see https://github.com/amiller/HoneyBadgerBFT/issues/59).
+
+    :param sid: session identifier
+    :param pid: my id number
+    :param N: the number of parties
+    :param f: the number of byzantine parties
+    :param coin: a ``common coin(r)`` is called to block until receiving a bit
+    :param input: ``input()`` is called to receive an input
+    :param decide: ``decide(0)`` or ``output(1)`` is eventually called
+    :param broadcast: broadcast channel
+    :param receive: receive channel
+    :return: blocks until
+    """
+    # Messages received are routed to either a shared coin, the broadcast, or AUX
+    est_values = defaultdict(lambda: [set(), set()])
+    aux_values = defaultdict(lambda: [set(), set()])
+    est_sent = defaultdict(lambda: [False, False])
+    bin_values = defaultdict(set)
+
+    # This event is triggered whenever bin_values or aux_values changes
+    bv_signal = Event()
+
+    def _recv():
+        while True:  # not finished[pid]:
+            (sender, msg) = receive()
+            logger.debug(f'receive {msg} from node {sender}',
+                          extra={'nodeid': pid, 'epoch': msg[1]})
+            assert sender in range(N)
+            if msg[0] == 'EST':
+                # BV_Broadcast message
+                _, r, v = msg
+                assert v in (0, 1)
+                if sender in est_values[r][v]:
+                    print('Redundant EST received', msg)
+                    raise RedundantMessageError(
+                        'Redundant EST received {}'.format(msg))
+
+                est_values[r][v].add(sender)
+                # Relay after reaching first threshold
+                if len(est_values[r][v]) >= f + 1 and not est_sent[r][v]:
+                    est_sent[r][v] = True
+                    for receiver in range(N):
+                        logger.debug(
+                            f"broadcast {('EST', r, v)} to node {receiver}",
+                            extra={'nodeid': pid, 'epoch': r})
+                        if receiver != 2:
+                            broadcast(('EST', r, v), receiver=receiver)
+
+                # Output after reaching second threshold
+                if len(est_values[r][v]) >= 2 * f + 1:
+                    logger.debug(
+                        f'add v = {v} to bin_value[{r}] = {bin_values[r]}',
+                        extra={'nodeid': pid, 'epoch': r},
+                    )
+                    bin_values[r].add(v)
+                    logger.debug(f'bin_values[{r}] is now: {bin_values[r]}',
+                                 extra={'nodeid': pid, 'epoch': r})
+                    bv_signal.set()
+
+            elif msg[0] == 'AUX':
+                # Aux message
+                _, r, v = msg
+                assert v in (0, 1)
+                if sender in aux_values[r][v]:
+                    print('Redundant AUX received', msg)
+                    raise RedundantMessageError(
+                        'Redundant AUX received {}'.format(msg))
+
+                aux_values[r][v].add(sender)
+                logger.debug(
+                    f'add v = {v} to aux_value[{r}] = {aux_values[r]}',
+                    extra={'nodeid': pid, 'epoch': r},
+                )
+
+                bv_signal.set()
+
+    # Run the receive loop in the background
+    _thread_recv = gevent.spawn(_recv)
+
+    # Block waiting for the input
+    vi = input()
+    assert vi in (0, 1)
+    est = vi
+    r = 0
+    already_decided = None
+    while True:  # Unbounded number of rounds
+        logger.debug(f'starting round {r} with est set to {est}', 
+                     extra={'nodeid': pid, 'epoch': r})
+        not_est = int(not bool(est))
+        if not est_sent[r][est]:
+            est_sent[r][est] = True
+            est_sent[r][not_est] = True
+            logger.debug(
+                f"broadcast {('EST', r, int(not bool(est)))} to node {0}",
+                extra={'nodeid': pid, 'epoch': r},
+            )
+            broadcast(('EST', r, int(not bool(est))), receiver=0)
+            logger.debug(
+                f"broadcast {('EST', r, est)} to node {1}",
+                extra={'nodeid': pid, 'epoch': r},
+            )
+            broadcast(('EST', r, est), receiver=1)
+
+        while len(bin_values[r]) == 0:
+            # Block until a value is output
+            bv_signal.clear()
+            bv_signal.wait()
+
+        w = next(iter(bin_values[r]))  # take an element
+        logger.debug(f"broadcast {('AUX', r, w)}",
+                     extra={'nodeid': pid, 'epoch': r})
+        for receiver in range(N):
+            if receiver != 2:
+                broadcast(('AUX', r, w), receiver=receiver)
+
+        # After this all messages within A are delivered and x sends both
+        # BVAL(0) and BVAL(1) to every node in A. Thus every node in A
+        # broadcasts both BVAL(0) and BVAL(1) and sets bin_values={0,1}.
+        logger.debug(
+            'x sends both BVAL(0) and BVAL(1) to every node in A.',
+            extra={'nodeid': pid, 'epoch': r},
+        )
+        broadcast(('EST', r, est), receiver=0)
+        broadcast(('EST', r, int(not bool(est))), receiver=1)
+
+        logger.debug(
+            f'Block until receiving the common coin value',
+            extra={'nodeid': pid, 'epoch': r},
+        )
+        # Block until receiving the common coin value
+        s = coin(r)
+        logger.debug(f's = coin(r) | s = {s}, r = {r}',
+                     extra={'nodeid': pid, 'epoch': r})
+        not_s = int(not bool(s))
+
+        logger.debug(f"broadcast {('EST', r, not_s)} to node 2",
+                     extra={'nodeid': pid, 'epoch': r})
+        broadcast(('EST', r, not_s), receiver=2)
+        logger.debug(f"broadcast {('AUX', r, not_s)} to node 2",
+                     extra={'nodeid': pid, 'epoch': r})
+        broadcast(('AUX', r, not_s), receiver=2)
+        logger.debug(f'exiting round {r}, setting est = s ({s})',
+                     extra={'nodeid': pid, 'epoch': r})
+        est = s
+        r += 1
--- test/test_binaryagreement.py
+++ test/test_binaryagreement.py
@@ -1,6 +1,8 @@
+import logging
 import unittest
 import gevent
 import random
+
 from gevent.event import Event
 from gevent.queue import Queue
 from honeybadgerbft.core.commoncoin import shared_coin
@@ -10,6 +12,7 @@ from collections import defaultdict
 
 from pytest import mark, raises
 
+logger = logging.getLogger(__name__)
 
 def simple_broadcast_router(N, maxdelay=0.005, seed=None):
     """Builds a set of connected channels, with random delay
@@ -80,6 +83,202 @@ def byzantine_broadcast_router(N, maxdelay=0.005, seed=None, **byzargs):
     def makeRecv(j):
         def _recv():
             (i,o) = queues[j].get()
+            return (i,o)
+
+        return _recv
+
+    return ([makeBroadcast(i) for i in range(N)],
+            [makeRecv(j) for j in range(N)])
+
+
+def network_scheduler(events, message_queues, receivers, rounds):
+    for r in rounds:
+        print(f'round {r}')
+        for i in range(8):
+            events[r, i].wait()
+            for m in message_queues[r, i]:
+                gevent.spawn_later(
+                    m.get('delay', 0.00000000000000000000001),
+                    receivers[m['receiver']].put,
+                    (m['sender'], m['msg']),
+                )
+            else:
+                events[r, i, 'done'].set()
+
+
+def byzantine_router_issue_59(N):
+    events = defaultdict(Event)
+    message_queues = defaultdict(Queue)
+    held_messages = defaultdict(set)
+    receivers = [Queue() for _ in range(N)]
+    counters = defaultdict(int)
+
+    coins = defaultdict(Queue)
+
+    r = 0
+    rounds = Queue()
+    rounds.put(r)
+    votes = {0: 0}
+
+    gevent.spawn(
+        network_scheduler,
+        events,
+        message_queues,
+        receivers,
+        rounds,
+    )
+
+    def makeBroadcast(i):
+        def _queue_messages(*, epoch, message_group,
+                            held_messages, message_queues):
+            phase = message_group['phase']
+            messages = message_group['messages']
+            counter = message_group['counter']
+            for m in messages:
+                if counters[epoch, phase] >= counter:
+                    break
+                try:
+                    held_messages[epoch].remove(m)
+                except KeyError:
+                    pass
+                else:
+                    message_queues[epoch, phase].put(
+                        {'sender': m[0], 'receiver': m[1], 'msg': m[2]}
+                    )
+                    counters[epoch, phase] += 1
+                    if phase == 4:
+                        coins[epoch].put(int(not bool(m[2][2])))
+                        votes[epoch + 1] = int(not bool(m[2][2]))
+                        break
+            if counters[epoch, phase] == counter:
+                message_queues[epoch, phase].put(StopIteration)
+
+        def _set_event(*, events, epoch, phase):
+            ready = not events[epoch, phase].ready()
+            if phase == 0 and epoch > 0:
+                ready = ready and events[epoch-1, 7, 'done'].ready()
+            elif phase > 0:
+                ready = ready and events[epoch, phase-1].ready()
+            if phase == 6:
+                ready = (ready and
+                    StopIteration in message_queues[epoch, phase].queue)
+            if ready:
+                print(f'event {phase}, round {epoch}')
+                events[epoch, phase].set()
+
+        def _send(j, o):
+            nonlocal r
+            v = votes[r]
+            not_v = int(not bool(v))
+
+            M0 = [
+                (3, 0, ('EST', r, not_v)),
+                (3, 1, ('EST', r, v)),
+                (2, 0, ('EST', r, not_v)),
+                (2, 1, ('EST', r, not_v)),
+                (0, 0, ('EST', r, v)),
+                (0, 0, ('EST', r, not_v)),
+            ]
+            M1 = [
+                (0, 1, ('EST', r, v)),
+                (1, 1, ('EST', r, v)),
+            ]
+            M2 = [
+                (1, 0, ('EST', r, v)),
+                (0, 1, ('EST', r, not_v)),
+                (0, 0, ('AUX', r, not_v)),
+                (0, 1, ('AUX', r, not_v)),
+                (1, 0, ('AUX', r, v)),
+                (1, 1, ('AUX', r, v)),
+            ]
+            M3 = [
+                (3, 0, ('EST', r, v)),
+                (3, 1, ('EST', r, not_v)),
+                (3, 0, ('AUX', r, not_v)),
+                (3, 1, ('AUX', r, not_v)),
+            ]
+            M4 = [
+                (3, 2, ('EST', r, 0)),
+                (3, 2, ('EST', r, 1)),
+            ]
+            G1 = (
+                {'phase': 0, 'messages': M0, 'counter': 6},
+                {'phase': 1, 'messages': M1, 'counter': 2},
+                {'phase': 2, 'messages': M2, 'counter': 6},
+                {'phase': 3, 'messages': M3, 'counter': 4},
+                {'phase': 4, 'messages': M4, 'counter': 1},
+            )
+
+            if i in (0, 1, 2, 3) and j == 3:
+                if r == 0 or events[r-1, 6].ready() and o[1] == r:
+                    gevent.spawn_later(
+                        0.00000000000000001, receivers[j].put, (i, o))
+            else:
+                held_messages[o[1]].add((i, j, o))
+
+            for message_group in G1:
+                _queue_messages(
+                    epoch=r,
+                    message_group=message_group,
+                    held_messages=held_messages,
+                    message_queues=message_queues,
+                )
+                _set_event(
+                    epoch=r, phase=message_group['phase'], events=events)
+
+            # PHASES 5 & 6
+            if not coins[r].empty():
+                not_coin = int(not bool(coins[r].peek()))
+                M5 = [
+                    (0, 2, ('EST', r, not_coin)),
+                    (1, 2, ('EST', r, not_coin)),
+                ]
+                M6 = [
+                    (0, 2, ('AUX', r, not_coin)),
+                    (1, 2, ('AUX', r, not_coin)),
+                    (2, 2, ('AUX', r, not_coin)),
+                    (3, 2, ('AUX', r, not_coin)),
+                ]
+                G2 = (
+                    {'phase': 5, 'messages': M5, 'counter': 2},
+                    {'phase': 6, 'messages': M6, 'counter': 3},
+                )
+                for message_group in G2:
+                    _queue_messages(
+                        epoch=r,
+                        message_group=message_group,
+                        held_messages=held_messages,
+                        message_queues=message_queues,
+                    )
+                    _set_event(
+                        epoch=r, phase=message_group['phase'], events=events)
+
+            if events[r, 6].ready():
+                for m in set(held_messages[r]):
+                    message_queues[r, 7].put(
+                        {'sender': m[0], 'receiver': m[1], 'msg': m[2]})
+                    held_messages[r].remove(m)
+                else:
+                    message_queues[r, 7].put(StopIteration)
+                    events[r, 7].set()
+
+            if events[r, 7].ready():
+                votes[r] = coins[r].peek()
+                r += 1
+                rounds.put(r)
+
+        def _bc(o, receiver=None):
+            if receiver is not None:
+                _send(receiver, o)
+            else:
+                for j in range(N):
+                    _send(j, o)
+
+        return _bc
+
+    def makeRecv(j):
+        def _recv():
+            (i,o) = receivers[j].get()
             return (i,o)
 
         return _recv
@@ -267,13 +466,6 @@ def test_binaryagreement():
     for i in range(5): _test_binaryagreement(seed=i)
 
 
-@mark.xfail(
-    raises=NotImplementedError,
-    reason='Place holder for https://github.com/amiller/HoneyBadgerBFT/issues/59')
-def test_issue59_attack():
-    raise NotImplementedError("Placeholder test failure for Issue #59")
-
-
 @mark.parametrize('values,s,already_decided,expected_est,'
                   'expected_already_decided,expected_output', (
     ({0}, 0, None, 0, 0, 0),
@@ -340,3 +532,57 @@ def test_set_next_round_estimate_raises(values, s, already_decided):
             already_decided=already_decided,
             decide=None,
         )
+
+        
+@mark.skip(
+    reason=('Will loop indefinitely as fix is not implemented yet.'
+            'See https://github.com/amiller/HoneyBadgerBFT/issues/59')
+)
+def test_issue59_attack():
+    from .byzantine import byz_ba_issue_59
+    N = 4
+    f = 1
+    seed = None
+    sid = 'sidA'
+    rnd = random.Random(seed)
+    sends, recvs = byzantine_router_issue_59(N)
+    threads = []
+    inputs = []
+    outputs = []
+
+    # Instantiate the common coin
+    coins_seed = rnd.random()
+    coins = _make_coins(sid+'COIN', N, f, coins_seed)
+
+    for i in range(4):
+        inputs.append(Queue())
+        outputs.append(Queue())
+
+    t = gevent.spawn(byz_ba_issue_59, sid, 3, N, f, coins[3],
+                     inputs[3].get, outputs[3].put_nowait, sends[3], recvs[3])
+    threads.append(t)
+
+    for i in (2, 0, 1):
+        t = gevent.spawn(binaryagreement, sid, i, N, f, coins[i],
+                         inputs[i].get, outputs[i].put_nowait, sends[i], recvs[i])
+        threads.append(t)
+
+    inputs[0].put(0)    # A_0
+    inputs[1].put(0)    # A_1
+    inputs[2].put(1)    # B
+    inputs[3].put(0)    # F (x)
+
+    #with raises(gevent.hub.LoopExit) as err:
+    #outs = [outputs[i].get() for i in range(N)]
+    failing = True
+    while failing:
+        try:
+            outs = [outputs[i].get() for i in range(N)]
+        except gevent.hub.LoopExit:
+            pass
+        else:
+            failing = False
+    try:
+        gevent.joinall(threads)
+    except gevent.hub.LoopExit:
+        pass

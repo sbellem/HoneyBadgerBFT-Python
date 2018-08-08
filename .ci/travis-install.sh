67e281d6427cb3c6ad666d62815bb24915ff485d
Sylvain Bellemare <sbellem@gmail.com>
2018-08-08 16:28:44 +0100

    fix

--- .ci/travis-install.sh
+++ .ci/travis-install.sh
@@ -8,10 +8,10 @@ cd charm && ./configure.sh && make install
 cd ..
 
 if [ "${BUILD}" == "tests" ]; then
-    pip install --process-dependency-links -e .[test]
+    pip install -e .[test]
     pip install --upgrade codecov
 elif [ "${BUILD}" == "flake8" ]; then
     pip install flake8
 elif [ "${BUILD}" == "docs" ]; then
-    pip install -e .[docs]
+    pip install --process-dependency-links -e .[docs]
 fi
